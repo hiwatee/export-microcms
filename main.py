846b1cdad13308bcaf39368ea6d95aba0c6fd06a
hiwatee <m-hiwatashi@kakusin-g.co.jp>
2020-12-18 14:21:31 +0900

    feat: use dotenv

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+API_ENDPOINT = os.environ.get("API_ENDPOINT")
+LIMIT = os.environ.get("LIMIT")
+
 def main():
-    print("")
+    print(API_ENDPOINT)
 
 if __name__ == "__main__":
     main()
